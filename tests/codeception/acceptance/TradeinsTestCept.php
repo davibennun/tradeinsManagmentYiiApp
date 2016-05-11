9310b17334fc8ed44bd6b14c2402ff630bf15301
Davi Nunes <davi@andradenunes.org>
2016-05-10 22:29:23 -0400

    create tradeinsTest cept with base feature set

---
+++ tests/codeception/acceptance/TradeinsTestCept.php
@@ -0,0 +1,9 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('list tradeins');
+$I->wantTo('navigate trough pages of tradeins');
+$I->wantTo('filter tradeis by name, watch, model, brand and value');
+$I->wantTo('view all details about a tradein');
+$I->wantTo('edit a tradein');
+$I->wantTo('delete a tradein');
+
