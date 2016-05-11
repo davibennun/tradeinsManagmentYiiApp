8e20cd6cd979e3e83fce62f1ba01e5b071271fe4
Davi Nunes <davi@andradenunes.org>
2016-05-10 22:32:31 -0400

    add operator steps

--- tests/codeception/acceptance/TradeinsTestCept.php
+++ tests/codeception/acceptance/TradeinsTestCept.php
@@ -1,5 +1,5 @@
 <?php 
-$I = new AcceptanceTester($scenario);
+$I = new \AcceptanceTester\OperatorSteps($scenario);
 $I->wantTo('list tradeins');
 $I->wantTo('navigate trough pages of tradeins');
 $I->wantTo('filter tradeis by name, watch, model, brand and value');
---
+++ tests/codeception/acceptance/_steps/OperatorSteps.php
@@ -0,0 +1,7 @@
+<?php
+namespace AcceptanceTester;
+
+class OperatorSteps extends \AcceptanceTester
+{
+
+}
