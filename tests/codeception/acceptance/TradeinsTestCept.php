9e600fd1fd66958a00f70b772ecc1e81892f9050
Davi Nunes <davi@andradenunes.org>
2016-05-11 00:38:50 -0400

    implementing tradein test case

--- tests/codeception/acceptance/TradeinsTestCept.php
+++ tests/codeception/acceptance/TradeinsTestCept.php
@@ -1,9 +1,44 @@
-<?php 
+<?php
 $I = new \AcceptanceTester\OperatorSteps($scenario);
-$I->wantTo('list tradeins');
-$I->wantTo('navigate trough pages of tradeins');
-$I->wantTo('filter tradeis by name, watch, model, brand and value');
-$I->wantTo('view all details about a tradein');
-$I->wantTo('edit a tradein');
-$I->wantTo('delete a tradein');
 
+$I->wantTo('list tradeins');
+    $tradeins = $I->haveAListOfTradeins(5);
+    $I->amInTradeinsListPage();
+    $I->seeTradeins($tradeins);
+
+$I->wantTo('navigate trough pages of tradeins');
+    $tradeins = $I->haveAListOfTradeins(100);
+    $I->amInTradeinsListPage();
+    $I->clickOnNextPage();
+    $I->seeTradein($tradeins[20]);
+
+$I->wantTo('filter tradeins by watch, model, brand and value');
+    $tradeins = $I->haveAListOfTradeins(100);
+    $tradein = $tradeins[0];
+    $I->amInTradeinsListPage();
+
+    $I->searchTradeinsBy('watch', $tradein->watch);
+    $I->onlySeeTradeinsWith('watch', $tradein->watch, $tradeins);
+
+    $I->searchTradeinsBy('model', $tradein->model);
+    $I->onlySeeTradeinsWith('model', $tradein->model, $tradeins);
+
+    $I->searchTradeinsBy('brand', $tradein->brand);
+    $I->onlySeeTradeinsWith('brand', $tradein->brand, $tradeins);
+
+    $I->searchTradeinsBy('value', $tradein->value);
+    $I->onlySeeTradeinsWith('value', $tradein->value, $tradeins);
+
+
+$I->wantTo('view all details about a tradein');
+    $tradein = $I->haveATradein();
+    $I->amInTradeinsDetailsPage($tradein->id);
+    $I->seeAllInfoAboutTheTradein($tradein);
+
+$I->wantTo('edit a tradein');
+    $tradein = $I->haveATradein();
+    $newTradein = $I->imagineATradein();
+    $I->amOnTradeinEditPage($tradein->id);
+    $I->fillTradeinForm($newTradein);
+    $I->submitTradeinForm();
+    $I->seeAllInfoAboutTheTradein($tradein);
--- tests/codeception/acceptance/_steps/OperatorSteps.php
+++ tests/codeception/acceptance/_steps/OperatorSteps.php
@@ -4,4 +4,105 @@ namespace AcceptanceTester;
 class OperatorSteps extends \AcceptanceTester
 {
 
+    function __construct()
+    {
+
+    }
+
+    public function amInTradeinsListPage()
+    {
+        $this->amOnPage('tradeins');
+    }
+
+
+    public function haveAListOfTradeins($int)
+    {
+    }
+
+    public function clickOnNextPage()
+    {
+        $this->click('.next-page');
+    }
+
+    public function seeTradeins($tradeins)
+    {
+        array_map([$this, 'seeTradein'], $tradeins);
+    }
+
+    public function dontSeeTradeins($tradeins)
+    {
+        array_map([$this, 'dontSeeTradein'], $tradeins);
+    }
+
+    public function seeTradein($tradein)
+    {
+        $this->see($tradein->watch, 'td');
+        $this->see($tradein->model, 'td');
+        $this->see($tradein->brand, 'td');
+    }
+
+    public function dontSeeTradein($tradein)
+    {
+        $this->dontSee($tradein->watch, 'td');
+        $this->dontSee($tradein->model, 'td');
+        $this->dontSee($tradein->brand, 'td');
+    }
+
+    public function haveATradein()
+    {
+    }
+
+    public function searchTradeinsBy($field, $value)
+    {
+        $this->fillField('search-field-'.$field, $value);
+    }
+
+    public function onlySeeTradeinsWith($field, $value, $tradeins)
+    {
+        //Get tradeins which does not contains the value we want to see
+        $tradeinsNotSupposedToSee = array_map(function($tradein) use ($value){
+            return $tradein->$field != $value;
+        }, $tradeins);
+
+        $this->seeTradein($value) && $this->dontSeeTradeins($tradeinsNotSupposedToSee);
+    }
+
+    public function amInTradeinsDetailsPage($id)
+    {
+        $this->amOnPage('tradeins/'.$id);
+    }
+
+    public function seeAllInfoAboutTheTradein($tradein)
+    {
+        $this->see($tradein->first_name);
+        $this->see($tradein->last_name);
+        $this->see($tradein->watch);
+        $this->see($tradein->model);
+        $this->see($tradein->brand);
+    }
+
+    public function imagineATradein()
+    {
+    }
+
+    public function amOnTradeinEditPage($tradeinId)
+    {
+        $this->am('tradein/update/', $tradeinId);
+    }
+
+    public function fillTradeinForm($newTradein)
+    {
+        $this->fillField('first_name', $newTradein->first_name);
+        $this->fillField('last_name', $newTradein->last_name);
+        $this->fillField('watch', $newTradein->watch);
+        $this->fillField('model', $newTradein->model);
+        $this->fillField('brand', $newTradein->brand);
+    }
+
+    public function submitTradeinForm()
+    {
+        $this->click('Submit');
+    }
+
+
 }
