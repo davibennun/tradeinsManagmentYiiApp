d1b69c7b63d56ceacf51963be6eb7134b61952f8
Davi Nunes <davi@andradenunes.org>
2016-05-24 11:50:26 -0400

    jomashop company name on footer

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -52,9 +52,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Joma Shop <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"></p>
     </div>
 </footer>
 
