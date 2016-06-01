8e77282a2c82c445adcff3cb221ab4dcbf5e1ca4
Davi Nunes <davi@andradenunes.org>
2016-06-01 09:55:33 -0400

    yii2 basic to advanced init

--- config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'jomaShopSoapUsername' => 'trade_in_test_1',
-    'jomaShopSoapApiKey' => '5s%3$_c>qWw7%.KQ'
-];
--- tests/codeception/acceptance/ListTradeinCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* @var $scenario Codeception\Scenario */
-$I = new \AcceptanceTester\OperatorSteps($scenario);
-
-$I->wantTo('List tradeins');
-$tradeins = $I->haveAListOfTradeins(5);
-$I->amInTradeinsListPage();
-$I->seeTradeins($tradeins);
