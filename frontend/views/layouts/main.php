76eb84890cc4c726ff65835e2524624491c81d1f
Davi Nunes <davi@andradenunes.org>
2016-06-02 09:59:45 -0400

    auth buttons on header

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -33,11 +33,25 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
+    $menuItems = [
+        ['label' => 'Trade-ins', 'url' => ['/tradein']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post')
+            . Html::submitButton(
+                'Logout (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link']
+            )
+            . Html::endForm()
+            . '</li>';
+    }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Trade-ins', 'url' => ['/tradein']],
-        ],
+        'items' => $menuItems,
     ]);
     NavBar::end();
     ?>
