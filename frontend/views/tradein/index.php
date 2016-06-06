2d7819e0122ae0c47841f7286e08fdd6b88d3c10
Davi Nunes <davi@andradenunes.org>
2016-06-06 14:05:08 -0400

    toolbar

--- frontend/views/tradein/index.php
+++ frontend/views/tradein/index.php
@@ -39,106 +39,33 @@ $exportColumns[] = [
         <?= Html::encode($this->title) ?>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-    <?= ExportMenu::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => array_merge($exportColumns),
-        'showColumnSelector' => false,
-        'exportConfig' => ['HTML' => false, 'TXT' => false, 'PDF' => false, 'Excel5' => false, 'Excel2007' => false],
-        'fontAwesome' => true,
-        'target' => ExportMenu::TARGET_SELF,
-        'dropdownOptions' => [
-            'label' => 'Export',
-            'class' => 'btn btn-default'
-        ]
-    ])
-    ?>
+
     </h1>
-    <hr/>
-    <?php
-        echo DynaGrid::widget([
+
+    <?=
+    DynaGrid::widget([
             'gridOptions' => [
                 'dataProvider' => $dataProvider,
                 'filterModel' => $searchModel,
                 'responsive' => true,
                 'hover' => true,
-                'export' => false,
+                'panel'=>[
+                    'heading'=>false
+                ],
+                'exportConfig' => [
+                    KartikGridView::CSV => ['label' => 'Save as CSV'],
+                    KartikGridView::EXCEL => ['label' => 'Save as Excel 2007+'],
+                    ],
+                'toolbar' => [
+                    ['content' => '{dynagrid}'],
+                    '{export}',
+                ]
             ],
             'options' => ['id'=>'dynagrid-tradein-index-1'],
-
+            'showPersonalize' => true,
             'columns' => [
-                [
-                    'class' => 'kartik\grid\ExpandRowColumn',
-                    'width' => '50px',
-                    'value' => function ($model, $key, $index, $column) {
-                        return \kartik\grid\GridView::ROW_COLLAPSED;
-                    },
-                    'detail' => function ($model, $key, $index, $column) {
-                        return Yii::$app->controller->renderPartial('_expand-row-details', ['model' => $model,'key'=>$key,'index'=>$index]);
-                    },
-                    'headerOptions' => ['class' => 'kartik-sheet-style'],
-                    'expandOneOnly' => true,
-                    'enableRowClick' => true
-                ],
-                [
-                    'attribute'=> 'status',
-                    'contentOptions' => function ($model, $key, $index, $column){
-                        return ['id' => 'td-tradein-'.$index.'-'.$column->attribute];
-                    },
-                    'format'=>'raw',
-                    'value' => function ($model, $key, $index, $widget){
-                        $labels = [
-                            '10' => '<span class="label label-primary">Active</span>',
-                            '20' => '<span class="label label-default">Inactive</span>',
-                            '30' => '<span class="label label-danger">Closed</span>',
-                            '40' => '<span class="label label-success">Successful</span>',
-                        ];
-                        return $labels[$model->status];
-                    },
-                    'filterType' => KartikGridView::FILTER_SELECT2,
-                    'filter' => ['10' => 'Active',
-                        '20' => 'Inactive',
-                        '30' => 'Closed',
-                        '40' => 'Successful',],
-                    'filterWidgetOptions' => [
-                        'hideSearch'=>true,
-                        'pluginOptions' => ['allowClear' => true],
-                    ],
-                    'filterInputOptions' => ['placeholder' => 'Select status'],
-                ],
-                [
-                    'attribute'=> 'first_name',
-                    'contentOptions' => function ($model, $key, $index, $column){
-                        return ['id' => 'td-tradein-'.$index.'-'.$column->attribute];
-                    }
-                ],
-                [
-                    'attribute'=> 'last_name',
-                    'contentOptions' => function ($model, $key, $index, $column){
-                        return ['id' => 'td-tradein-'.$index.'-'.$column->attribute];
-                    }
-                ],
-                [
-                    'attribute'=>'first_contact',
-                    'format'=> ['date', 'php:m-d-Y'],
-                    'contentOptions' => function ($model, $key, $index, $column) {
-                        return ['id' => 'td-tradein-' . $index . '-' . $column->attribute];
-                    }
-                ],
-                [
-                    'attribute'=>'last_contact',
-                    'format'=> ['date', 'php:m-d-Y'],
-                    'contentOptions' => function ($model, $key, $index, $column) {
-                        return ['id' => 'td-tradein-' . $index . '-' . $column->attribute];
-                    }
-                ],
-                [
-                    'attribute'=>'model_number',
-                    'contentOptions' => function ($model, $key, $index, $column) {
-                        return ['id' => 'td-tradein-' . $index . '-' . $column->attribute];
-                    }
-                ],
-            ],
-
+                'first_name','last_name'
+            ]
 
         ]);
     ?>
